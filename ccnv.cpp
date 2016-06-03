0b0b60bfa82dc1789ba5e2f04a9aae26fdb51516
mahulchak <mchakrab@uci.edu>
2016-06-02 19:28:30 -0700

    added features

--- ccnv.cpp
+++ ccnv.cpp
@@ -11,14 +11,15 @@ using namespace std;
 int main(int argc, char * argv[])
 {
         if(argc==1)
-        {cerr<<"Usage: "<<argv[0]<<"-d1 delta_test.out -d2 delta_ref.out -q query.list"<<endl;
-        exit(EXIT_FAILURE);
+        {
+		cerr<<"Usage: "<<argv[0]<<" -d1 delta_candidate.out -d2 delta_ref.out -q query_name.list -c cutoff_for_repeats -D master_delta.out"<<endl;
+        	exit(EXIT_FAILURE);
         }
 
-	ifstream delta1,delta2,fin;
-	
+	ifstream Delta,delta1,delta2,fin;
+	ofstream fout,chromMap,unmapped;	
 
-	asmMerge merge,merge1; 
+	asmMerge master,merge,merge1; 
 	
 	string header,ref_name,qu_name,tempname,name;
 	
@@ -26,8 +27,55 @@ int main(int argc, char * argv[])
 	int qu_end = 0;
 	int r_st = 0;
 	int r_end = 0;
+	int copyCutoff = 0;
 	
-	delta1.open(argv[2]);
+	copyCutoff = strtod(argv[8],NULL);
+
+//cout<<string(argv[8])<<endl;
+	if(string(argv[10]) != "")
+	{
+		Delta.open(argv[10]);
+		while(getline(Delta,header))
+       		{
+        	        if(header[0] =='>')
+                	{
+
+                        	ref_name = xtractcol(header,' ',1);
+	                        ref_name = ref_name.substr(1); //blast adds |c| so remove >|c|
+        	                master.r_name.push_back(ref_name);
+                	        qu_name = xtractcol(header,' ',2);
+                	        master.q_name.push_back(qu_name);
+                	        tempname = ref_name.append(qu_name); // tempname is the index for the map. they describe alignment pairs( e.g. BackboneX ctgY). should be unique.
+                       		master.ref_len[tempname] = atoi(xtractcol(header,' ',3).c_str());
+                	        master.q_len[tempname] = atoi(xtractcol(header,' ',4).c_str());
+               		}
+
+       		        if(header[0] != '>' && header.size()>10)
+               		{
+                       		r_st = atoi(xtractcol(header,' ',1).c_str());
+                     		master.ref_st[tempname].push_back(r_st); // storing the coordinates for each alignment hit
+               		      	r_end = atoi(xtractcol(header,' ',2).c_str());
+                  		master.ref_end[tempname].push_back(r_end);
+                     		qu_st = atoi(xtractcol(header,' ',3).c_str());
+                  		master.q_st[tempname].push_back(qu_st);
+                  		qu_end = atoi(xtractcol(header,' ',4).c_str());
+                     		master.q_end[tempname].push_back(qu_end);
+              		 }
+       		}
+	
+
+		Delta.close();
+	
+		ovlStoreCalculator(master);//calculate the alignment length for each alignment
+		findChromPartner(master);	
+		chromMap.open("ctgmap.txt");
+		for(map<string,string>::iterator it=master.storeHomolog.begin();it!= master.storeHomolog.end();it++)
+		{
+			chromMap<< it->first << "\t"<< it->second <<endl;
+		}
+		chromMap.close();
+	}
+	delta1.open(argv[2]);//open the second delta file
 
 	while(getline(delta1,header))
 	{
@@ -35,7 +83,7 @@ int main(int argc, char * argv[])
              	   {
 		
             		ref_name = xtractcol(header,' ',1);	
-			ref_name = ref_name.substr(4); //blast adds |c| so remove >|c|
+			ref_name = ref_name.substr(5); //blast adds |c| so remove >|c|
 			merge.r_name.push_back(ref_name); 
 			qu_name = xtractcol(header,' ',2); 
 			merge.q_name.push_back(qu_name);
@@ -64,7 +112,7 @@ int main(int argc, char * argv[])
                    {
 
                         ref_name = xtractcol(header,' ',1);
-                        ref_name = ref_name.substr(4);
+                        ref_name = ref_name.substr(5);
                         merge1.r_name.push_back(ref_name);
                         qu_name = xtractcol(header,' ',2);
 //cout<<qu_name<<endl;
@@ -88,7 +136,8 @@ int main(int argc, char * argv[])
 	}
 	delta2.close();
 	fin.open(argv[6]);
-	
+collapseRange(merge);
+collapseRange(merge1);	
 	
 	while(getline(fin,header))
 	{
@@ -101,17 +150,32 @@ int main(int argc, char * argv[])
 	{
 		countCopy(header,merge1);
 	}
-
+	fout.open("cnv_report.tsv");
+	unmapped.open("unmapped.seq");
 	for(map<string,int>::iterator it=merge.storeCount.begin();it!= merge.storeCount.end();it++)
 	{
-	 if((it->second) != merge1.storeCount[it->first])
+		 if(((it->second) > merge1.storeCount[it->first]) && (it->second < copyCutoff)) //if former has more copies.copyCutoff is for filtering TEs and repeats
 		{
 		cout<<it->first<<"\t"<<it->second<<"\t"<<merge1.storeCount[it->first]<<endl;
+			for(unsigned i=0;i<merge.storeName[it->first].size();i++) //access the query names 
+			{
+				tempname = merge.storeName[it->first][i]; //create the index
+				for(unsigned j =0;j<merge.q_st[tempname].size();j++)
+				{
+					fout<<tempname<<"\t"<<merge.q_st[tempname][j]<<"\t"<<merge.q_end[tempname][j]<<"\t"<<it->second<<"\t"<<merge1.storeCount[it->first]<<endl;
+				}
+			}
 		}
+		if((it->second ==0)||(merge1.storeCount[it->first]==0))
+		{
+			unmapped<<it->first<<"\t"<<it->second<<"\t"<<merge1.storeCount[it->first]<<endl;
+		}
+	
 	}
 	
+	
 	fin.close();
-
-
+	fout.close();
+	unmapped.close();
 	return 0;
 }
--- cnvlib.cpp
+++ cnvlib.cpp
@@ -25,24 +25,104 @@ void countCopy(string & str, asmMerge & merge)
 {
 	string tempname;
 	int tot_count = 0;
+
 	for(unsigned int i = 0;i<merge.r_name.size();i++)
 	{
 		tempname = merge.r_name[i] + merge.q_name[i];
 
 		if(tempname.find(str) != string::npos)
 		{
+		merge.seqCount[str]++;
 			for(unsigned int j=0;j<merge.ref_st[tempname].size();j++)
 			{
-				if((merge.ref_end[tempname][j] - merge.ref_st[tempname][j]) > int(merge.ref_len[tempname]*0.5)) //the length of the duplicates have to be at least half
+				if((merge.ref_end[tempname][j] - merge.ref_st[tempname][j]) > int(merge.ref_len[tempname]*0.75)) //the length of the duplicates have to be at least half
 				{
 					//tot_count = merge.ref_st[tempname].size() + tot_count;
 					tot_count++;
-			//cout<< merge.r_name[i]<<"\t"<<merge.q_name[i]<<"\t"<<merge.ref_st[tempname].size()<<endl;
 				}
 			}
+		merge.storeName[str].push_back(tempname);
 		}
 	}
 
 	merge.storeCount[str] = tot_count;
-}
 	
+}
+////////////////////////////////////////////////////////////////////////////////
+void collapseRange(asmMerge & merge)
+{
+	string tempname;
+	int len =0;
+	
+	for(unsigned int i =0; i<merge.r_name.size();i++)
+	{	
+		tempname = merge.r_name[i] + merge.q_name[i];
+		len = merge.ref_len[tempname];
+		for(unsigned j = 1; j<merge.ref_st[tempname].size();j++)
+		{
+			if((chkOvl(merge,tempname,j) == 0) && ((merge.ref_end[tempname][j]-merge.ref_st[tempname][j-1])<len)) // if the ranges don't overlap
+			{
+				merge.ref_end[tempname][j-1] = merge.ref_end[tempname][j];
+				merge.q_end[tempname][j-1] = merge.q_end[tempname][j];
+				merge.ref_st[tempname].erase(merge.ref_st[tempname].begin()+j);
+				merge.ref_end[tempname].erase(merge.ref_end[tempname].begin()+j);
+				merge.q_st[tempname].erase(merge.q_st[tempname].begin()+j);
+				merge.q_end[tempname].erase(merge.q_end[tempname].begin()+j);
+				j--;
+			}
+		}
+	}
+}
+///////////////////////////////////////////////////////////////////////////////
+bool chkOvl(asmMerge & merge, string & str,unsigned int & j)
+{
+	int end1 = merge.ref_end[str][j-1];
+	int st2 = merge.ref_st[str][j];
+		
+	if((!(st2 < end1)) && (st2 - end1 <int(merge.ref_len[str]*0.12))) // the gap between the ranges has to be less than 12% of the reference length
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+	 	
+}
+/////////////////////////////////////////////////////////////////////////////
+int ovlCalculator(vector<int>& q_st, vector<int>& q_end)
+{
+int ovl = 0;
+
+        for(unsigned int j =0;j<q_st.size();j++)
+        {
+        ovl = ovl + abs(q_st[j] - q_end[j]);
+        }
+
+
+return ovl;
+}
+/////////////////////////////////////////////////////////////////////////////////////
+void ovlStoreCalculator(asmMerge & merge)
+{
+	string tempname;
+		for(unsigned int i =0;i<merge.r_name.size();i++)
+		{
+			tempname = merge.r_name[i]+merge.q_name[i];
+			merge.ovlStore[tempname] = ovlCalculator(merge.q_st[tempname],merge.q_end[tempname]);
+		}
+}
+////////////////////////////////////////////////////////////////////////////////////
+void findChromPartner(asmMerge & merge)
+{
+	string tempname;
+	for(unsigned int i =0;i<merge.q_name.size();i++)
+	{
+		tempname = merge.r_name[i] + merge.q_name[i];
+		if(merge.storeHomAln[merge.q_name[i]] < merge.ovlStore[tempname]) //if found a longer alignment
+		{
+			merge.storeHomolog[merge.q_name[i]] = merge.r_name[i];
+			merge.storeHomAln[merge.q_name[i]] = merge.ovlStore[tempname];
+		}
+	}
+}		                                                                
